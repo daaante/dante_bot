035e7a16539f9a96f3735d66139ebfd1dbabce6f
daaante <blaackmonsterxxx@gmail.com>
2016-06-02 16:28:59 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {207305009},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
